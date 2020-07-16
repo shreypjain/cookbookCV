4b92d4a4f2f20ca9853f1080e1016091394fb172
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-07-16 14:46:00 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,30 +1,5 @@
 import pandas as pd
 import numpy as np
-#data/recipes.csv
-df = pd.read_csv('data/recipes.csv', index_col=0)
-
-def get_recipes(inputs):
-    for list_ingreds, list_title, list_instructions in zip(df['ingredients'].tolist(), df.index.tolist(), df['instructions'].tolist()):
-        split_ingreds = str(list_ingreds)
-        split_ingreds = split_ingreds.split()
-        if all(x in split_ingreds for x in inputs):
-            print ('\nThe title of the dish is: ')
-            print (list_title)
-    
-            print(f'\nThe List of ingredients for {list_title} are: ')
-            print (list_ingreds)
-            
-            print (f'\nThe instructions to cook {list_title} are: ')
-            print (list_instructions)
-            print('-'*50)
-            
-        else:
-            continue
-
-available = ['Apple', 'Chicken']
-get_recipes(inputs = available)
-import pandas as pd
-import numpy as np
 
 df = pd.read_csv('../data/recipes.csv')
 
@@ -65,6 +40,6 @@ def get_recipes(inputs):
     else:
         print (f'Sorry there are no recipes for {inputs}')
 
-available = ['Onion', 'Cheese', 'Water']
+available = ['Water', 'Oil', 'Bread', 'Garlic']
 
 get_recipes(inputs = available)
