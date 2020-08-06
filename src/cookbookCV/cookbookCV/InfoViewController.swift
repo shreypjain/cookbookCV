b1b003f13b510d94efd30ab9703d56c1798814cd
Shrey Jain <jainshreyp@gmail.com>
2020-08-06 10:56:22 -0400

    finished the info page

--- src/cookbookCV/cookbookCV/InfoViewController.swift
+++ src/cookbookCV/cookbookCV/InfoViewController.swift
@@ -30,8 +30,8 @@ class InfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
     
 
